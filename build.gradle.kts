0dde25a9888aa2d41ba15ebe3ecadecc8c73ab2e
blackwilliam <lw2957409438@gmail.com>
2022-10-15 10:42:47 +0800

    2022-10-15 10:42 commit

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,6 +2,7 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
     kotlin("jvm") version "1.7.20"
+    kotlin("plugin.serialization") version "1.7.20"
     application
 }
 
@@ -16,6 +17,7 @@ dependencies {
     // https://mvnrepository.com/artifact/org.jsoup/jsoup
     implementation("org.jsoup:jsoup:1.15.3")
     testImplementation(kotlin("test"))
+    implementation("com.charleskorn.kaml:kaml:0.49.0")
 }
 
 tasks.test {
--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,11 +1,21 @@
+import com.charleskorn.kaml.Yaml
+import kotlinx.serialization.Serializable
 import org.jsoup.Jsoup
 import java.io.File
-import java.lang.reflect.Field
+import java.lang.StringBuilder
 import java.net.URL
 import java.nio.file.Files
+import java.nio.file.Path
 import java.nio.file.Paths
+import kotlin.collections.ArrayList
 import kotlin.io.path.Path
 import kotlin.io.path.deleteExisting
+import kotlin.io.path.deleteIfExists
+
+@Serializable
+data class Times (
+    var time: List<String>
+)
 
 fun downloadFile(url: String, fileName: String) {
     val download = URL(url)
@@ -13,6 +23,8 @@ fun downloadFile(url: String, fileName: String) {
 }
 
 fun main(args: Array<String>) {
+    val currentPath = Paths.get(System.getProperty("user.dir")).toString()
+
     val home =
         Jsoup.connect("https://clashnode.com/f/freenode")
             .timeout(0)
@@ -21,17 +33,52 @@ fun main(args: Array<String>) {
     val list = home.getElementsByClass("post-list").get(0)
     val elem = list.getElementsByTag("li").get(0)
     val link = elem.getElementsByTag("a").get(0).attr("href")
+    val time = elem.getElementsByTag("a").get(2).text()
+    val regex = Regex("""\d*月\d*日""")
+    val currenttime =
+        regex.find(time)?.value?.
+                replace("月", "-")?.
+                replace("日", "")
+    println("current time: $currenttime")
 
-    val nodes = Jsoup.connect(link).get()
-    val ps = nodes.getElementsByTag("p")
-    val clashnodes = ps.get(10).text()
+    val timefile = currentPath + "/currenttime.yaml"
+    val parsedYaml =
+        Yaml.default.decodeFromString(Times.serializer(),
+            Path.of(timefile).toFile().readText())
+    val lasttime = parsedYaml.time[0]
+
+    if (lasttime != currenttime) {
+        val clashnodespath = currentPath + "/clashnodes.yaml"
+
+        val yesterdayclashnodepath =
+                        currentPath + "/clashnodes-" + lasttime + ".yaml"
+        if (!File(yesterdayclashnodepath).exists()){
+            val success =
+                File(clashnodespath).renameTo(File(yesterdayclashnodepath))
+
+            if (success) {
+                println("clashnodes backup succeeded!")
+                val timelist = ArrayList<String>()
+                timelist.add(currenttime.toString())
+                parsedYaml.time = timelist
+
+                val result =
+                    Yaml.default.encodeToString(Times.serializer(), parsedYaml)
+                println(result)
+                File(timefile).writeText(result)
+
+                val nodes = Jsoup.connect(link).get()
+                val ps = nodes.getElementsByTag("p")
+                val clashnodes = ps.get(10).text()
+
+                Path(clashnodespath).deleteIfExists()
+                downloadFile(clashnodes, clashnodespath)
+            }
+        }
+    }
+
 //    val currenttime = System.currentTimeMillis().toString()
 
-    val currentPath = Paths.get(System.getProperty("user.dir"))
 //    val filepath = currentPath.toString() +
 //            "/clashnodes-" + currenttime + ".yaml"
-    val filepath = currentPath.toString() + "/clashnodes.yaml"
-
-    Path(filepath).deleteExisting()
-    downloadFile(clashnodes, filepath)
 }
